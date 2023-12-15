5b492ac65b5adfeb94e60d22708a1791a5a8bc2e
ScottShadow <rudaseswascottmc@gmail.com>
2023-12-15 00:28:03 +0000

    push error handling

--- opcodes.c
+++ opcodes.c
@@ -48,6 +48,7 @@ void pall(stack_t **stack, unsigned int line_number)
 	if (!h)
 	{
 		fprintf(stderr, "L%d: can't pall, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
 	}
 	while (h)
 	{
@@ -72,6 +73,7 @@ void pint(stack_t **stack, unsigned int line_number)
 	else
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
 	}
 }
 /**
